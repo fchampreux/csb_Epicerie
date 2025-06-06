c1cd062bd0f52924d46f4072d005c2cb1df7e2ee
Frédéric CHAMPREUX <fchampreux@gmail.com>
2025-06-06 09:33:09 +0000

    Inserted Burger Menu as demonstrated in https://www.codeur.com/tuto/css/menu-burger-html-css/

--- src/script/script.js
+++ src/script/script.js
@@ -1,16 +1,33 @@
 function currentSlide(number) {
-	let slide = document.querySelector("#item-"+number);
-	slide.checked = true;
-	return number;
+  let slide = document.querySelector("#item-" + number);
+  slide.checked = true;
+  return number;
 }
 
 let timer = 3000;
-setInterval(function(){
-	let count = document.querySelectorAll('input[name="slider"]').length;
-	let checked = document.querySelector('input[name="slider"]:checked');
-	let nb = (parseInt(checked.id.slice(5))+1)%(count+1);
-	if (nb == 0){
-        nb = nb + 1;
-	}
-	currentSlide(nb);
+setInterval(function () {
+  let count = document.querySelectorAll('input[name="slider"]').length;
+  let checked = document.querySelector('input[name="slider"]:checked');
+  let nb = (parseInt(checked.id.slice(5)) + 1) % (count + 1);
+  if (nb == 0) {
+    nb = nb + 1;
+  }
+  currentSlide(nb);
 }, timer);
+
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
